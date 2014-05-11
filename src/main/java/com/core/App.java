f0cf24b83f236bcec92520b53725e82f6cfdf161
nicolas moraes <nico.moraes@gmail.com>
2014-05-11 02:55:36 -0300

    http get and script para guardar en mongo

--- src/main/java/com/core/App.java
+++ src/main/java/com/core/App.java
@@ -1,7 +1,25 @@
 package com.core;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.net.URL;
 import java.net.UnknownHostException;
+import java.nio.charset.Charset;
 import java.util.Date;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+
+import net.sf.json.JSONArray;
+import net.sf.json.JSONException;
+import net.sf.json.JSONObject;
+import net.sf.json.JSONSerializer;
 
 import com.mongodb.BasicDBObject;
 import com.mongodb.DB;
@@ -37,6 +55,128 @@ public class App {
 		
 	}
 	
+
+
+	
+	public static void buscarItem(String parametro){
+		
+		 
+		String site = null;
+		String query = null;
+		String currency = null;
+		String stop_time = null;
+		String condition = null;
+
+		String titles = null;
+		String subtitle = null;
+		String price = null;
+		String thumbnail = null;
+		String pic = null;
+		//String parametro = name.replaceAll(" ", "");
+		String html = "";
+		String id= null;
+		//System.out.println(parametro);
+		String symbolCurrency=null;
+		String [] s = new String[50];
+		
+		
+		DB db = mongo.getDB("nicoDB");
+		
+		/**** Get collection / table from 'testdb' ****/
+		// if collection doesn't exists, MongoDB will create it for you
+		DBCollection table = db.getCollection("coleccion_ipod");
+		
+		
+		try {
+		
+			
+			HttpClient client = new DefaultHttpClient();
+		    
+		    HttpGet get = new HttpGet("https://api.mercadolibre.com/sites/MLU/search?q="+ parametro);
+
+		    HttpResponse response = client.execute(get);
+		    BufferedReader rd = new BufferedReader(new InputStreamReader(response.getEntity().getContent()));
+
+		    StringBuffer result = new StringBuffer();
+		    String line = "";
+		    while ((line = rd.readLine()) != null) {
+		        result.append(line);
+		    }
+
+		    JSONObject json =(JSONObject) JSONSerializer.toJSON(result.toString());
+		    
+			site = json.getString("site_id");
+			query = json.getString("query");
+
+			// Obtengo el array de items
+			JSONObject p = (JSONObject) json.get("paging");
+			String total = p.getString("total");
+			System.out.println("Total: " + total);
+
+			JSONArray results = json.getJSONArray("results");
+			
+			System.out.println(results);
+			//recorro el array de resultados.
+			//for (int i = 0; i < results.size(); i++) {
+				for (int i = 0; i < 10; i++) {
+
+				JSONObject array = (JSONObject) results.get(i);
+				titles = array.getString("title");
+
+				/**** Insert ****/
+				// create a document to store key and value
+				BasicDBObject document = new BasicDBObject();
+				document.put("_id", array.getString("id"));
+				document.put("site_id", array.getString("site_id"));
+				document.put("title", array.getString("title"));
+				subtitle = array.getString("subtitle");
+				if(subtitle.equals("null")){
+					subtitle= "";
+				}else{
+					document.put("subtitle", subtitle);
+					}
+				
+				document.put("price", array.getString("price"));
+				document.put("permalink", array.getString("permalink"));
+				document.put("thumbnail", array.getString("thumbnail"));
+				document.put("currency_id", array.getString("currency_id"));
+			
+				table.insert(document);
+						
+				
+			}
+		
+
+
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+
+	}
+	
+	
+	public void insertDocument(String collection, String doc){
+		
+		DB db = mongo.getDB("nicoDB");
+		
+		/**** Get collection / table from 'testdb' ****/
+		// if collection doesn't exists, MongoDB will create it for you
+		DBCollection table = db.getCollection(collection);
+		
+		/**** Insert ****/
+		// create a document to store key and value
+		BasicDBObject document = new BasicDBObject();
+		document.put("_id", "nico.tef22@gmail.com");
+		document.put("name", "nicolas");
+		document.put("age", 30);
+		document.put("telefono", "093818108");
+		document.put("createdDate", new Date());
+		table.insert(document);
+		
+	}
+	
 	
 	
 	
@@ -60,13 +200,13 @@ public class App {
 			
 			/**** Insert ****/
 			// create a document to store key and value
-			BasicDBObject document = new BasicDBObject();
-			document.put("_id", "nico.tef2@gmail.com");
+			/*BasicDBObject document = new BasicDBObject();
+			document.put("_id", "nico.tef22@gmail.com");
 			document.put("name", "nicolas");
 			document.put("age", 30);
 			document.put("telefono", "093818108");
 			document.put("createdDate", new Date());
-			table.insert(document);
+			table.insert(document);*/
 			
 			
 			/**** Find and display ****/
@@ -99,6 +239,8 @@ public class App {
 			System.out.println("Login is failed!");
 			
 		}
+
+			buscarItem("ipad");
 		
 	}
 }
